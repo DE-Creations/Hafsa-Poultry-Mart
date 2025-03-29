9ac101250cf9a2ae576b05ca4437f642b13220c5
Amindu-Dulanjana <umamidu@gmail.com>
2025-03-29 17:14:20 +0530

    customers changes

---
+++ resources/views/pages/customers/index.blade.php
@@ -0,0 +1,150 @@
+<x-app-layout>
+        <!-- App body starts -->
+        <div class="app-body">
+
+            <!-- Container starts -->
+            <div class="container">
+  
+              <!-- Row start -->
+              <div class="row gx-3">
+                <div class="col-12 col-xl-6">
+                  <!-- Breadcrumb start -->
+                  <ol class="breadcrumb mb-3">
+                    <li class="breadcrumb-item">
+                      <i class="icon-house_siding lh-1"></i>
+                      <a href="index.html" class="text-decoration-none">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">Clients</li>
+                  </ol>
+                  <!-- Breadcrumb end -->
+                </div>
+              </div>
+              <!-- Row end -->
+  
+              <!-- Row start -->
+              <div class="row gx-3">
+                <div class="col-12">
+                  <div class="card mb-3">
+                    <div class="card-header">
+                      <h5 class="card-title">Clients List</h5>
+                    </div>
+                    <div class="card-body">
+                      <!-- Search container start -->
+                      <div class="row">
+                        <div class="col-sm-4 col-12">
+                          <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Client Name" />
+                            <button class="btn btn-info" type="button">
+                              Search
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- Search container end -->
+  
+                      <!-- Contacts Container Start -->
+                      <div class="contacts-container mt-3">
+                        <div class="contact-list">
+                          <div class="bg-light px-3 py-2 m-3 mb-1 rounded-2">
+                            A
+                          </div>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user.png" alt="Bootstrap Gallery" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Angelica Ramos</h6>
+                              <small class="opacity-50">Software Engineer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user2.png" alt="Bootstrap Gallery" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Brenden Wagner</h6>
+                              <small class="opacity-50">Chief Operating Officer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user3.png" alt="Bootstrap Themes" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Cedric Kelly</h6>
+                              <small class="opacity-50">Senior Javascript Developer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user4.png" alt="Bootstrap Dashboards" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Howard Hatfield</h6>
+                              <small class="opacity-50">Senior Marketing Designer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user4.png" alt="Bootstrap Themes" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Jenette Caldwell</h6>
+                              <small class="opacity-50">Senior Marketing Designer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user5.png" alt="Bootstrap Dashboards" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Olivia Liang</h6>
+                              <small class="opacity-50">Systems Administrator</small>
+                            </div>
+                          </a>
+                          <div class="bg-light px-3 py-2 m-3 mb-1 rounded-2">
+                            B
+                          </div>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user.png" alt="Bootstrap Themes" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Angelica Ramos</h6>
+                              <small class="opacity-50">Software Engineer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user2.png" alt="Bootstrap Admin Themes"
+                              class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Paul Byrd</h6>
+                              <small class="opacity-50">Marketing Designer</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user3.png" alt="Bootstrap Gallery" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Sonya Frost</h6>
+                              <small class="opacity-50">Senior Javascript Developer</small>
+                            </div>
+                          </a>
+                          <div class="bg-light px-3 py-2 m-3 mb-1 rounded-2">
+                            C
+                          </div>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user5.png" alt="Bootstrap Admin Themes"
+                              class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Angelica Ramos</h6>
+                              <small class="opacity-50">Technical Author</small>
+                            </div>
+                          </a>
+                          <a href="javascript:void(0)" class="px-3 py-2 d-flex align-items-center gap-3">
+                            <img src="assets/images/user1.png" alt="Bootstrap Gallery" class="img-3x rounded-circle" />
+                            <div class="flex-1 flex flex-col">
+                              <h6 class="fw-bold m-0">Brenden Wagner</h6>
+                              <small class="opacity-50">Javascript Developer</small>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <!-- Contacts Container End -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Row end -->
+  
+            </div>
+            <!-- Container ends -->
+  
+          </div>
+          <!-- App body ends -->
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\CustomersController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [CustomersController::class, 'index'])->name('customers');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
