784f5c3b1ddaf1bf6c598cbcb6545cfa85fab910
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-04-04 22:23:04 +0530

    changes

--- resources/views/components/main/navbar.blade.php
+++ resources/views/components/main/navbar.blade.php
@@ -29,178 +29,22 @@
                         <i class="icon-support_agent"></i>GRN
                     </a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <i class="icon-package"></i>Pages
+                <li class="nav-item {{ request()->routeIs('expenses*') ? 'active-link' : '' }}">
+                    <a class="nav-link" href="{{ route('expenses.index') }}">
+                        <i class="icon-support_agent"></i>Expenses
                     </a>
-                    <ul class="dropdown-menu dropdown-megamenu">
-                        <li>
-                            <a class="dropdown-item" href="agent-profile.html">
-                                <span>Agent Profile</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="starter-page.html">
-                                <span>Starter Page</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="client-list.html">
-                                <span>Client List</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="create-invoice.html">
-                                <span>Create Invoice</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="invoice.html">
-                                <span>Invoice Details</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="invoice-list.html">
-                                <span>Invoice List</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="faq.html">
-                                <span>FAQ</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="contact-us.html">
-                                <span>Contact Us</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="notifications.html">
-                                <span>Notifications</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="subscribers.html">
-                                <span>Subscribers</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="placeholder.html">
-                                <span>Placeholder</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="account-settings.html">
-                                <span>Account Settings</span></a>
-                        </li>
-                    </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        <i class="icon-server"></i>UI Elements
-                    </a>
-                    <ul class="dropdown-menu dropdown-megamenu">
-                        <li>
-                            <a class="dropdown-item" href="accordions.html">
-                                <span>Accordions</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="alerts.html">
-                                <span>Alerts</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="buttons.html">
-                                <span>Buttons</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="badges.html">
-                                <span>Badges</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="cards.html">
-                                <span>Cards</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="custom-cards.html">
-                                <span>Custom Cards</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="carousel.html">
-                                <span>Carousel</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="icons.html">
-                                <span>Icons</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="list-items.html">
-                                <span>List Items</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="modals.html">
-                                <span>Modals</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="progress.html">
-                                <span>Progress Bars</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="popovers.html">
-                                <span>Popovers</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="tables.html">
-                                <span>Tables</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="tabs.html">
-                                <span>Tabs</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="tooltips.html">
-                                <span>Tooltips</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="typography.html">
-                                <span>Typography</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <i class="icon-drive_file_rename_outline"></i>Forms
+                        <i class="icon-drive_file_rename_outline"></i>Reports
                     </a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a class="dropdown-item" href="form-inputs.html"><span>Basic Inputs</span></a>
+                            <a class="dropdown-item" href="form-inputs.html"><span>Expenses</span></a>
                         </li>
                         <li>
-                            <a class="dropdown-item" href="form-checkbox-radio.html"><span>Checkbox &amp;
-                                    Radio</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="form-file-input.html"><span>File
-                                    Input</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="form-validations.html"><span>Validations</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="form-layouts.html">Form Layouts</a>
+                            <a class="dropdown-item" href="form-checkbox-radio.html"><span>Profit &amp; loss</span></a>
                         </li>
                     </ul>
                 </li>
