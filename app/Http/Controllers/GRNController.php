3fe14b97041cd52faf051edcbcc57526c9e9baec
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-04-04 17:07:47 +0530

    changes

---
+++ app/Http/Controllers/GRNController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GRNController extends ParentController
+{
+    public function index()
+    {
+        return view('pages.grn.index');
+    }
+}
