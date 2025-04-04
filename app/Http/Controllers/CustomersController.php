21a7818f0a872e1a675b64de194ff377b4e98b5e
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-04-05 00:44:52 +0530

    customer changes

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Customer\StoreCustomerRequest;
+use domain\facades\CustomerFacade\CustomerFacade;
 use Illuminate\Http\Request;
 
 class CustomersController extends ParentController
 {
     public function index()
     {
-        return view('pages.customers.index');
+        $customers = CustomerFacade::getCustomers();
+        return view('pages.customers.index', [
+            'customers' => $customers,
+        ]);
     }
 
-    public function store(){
-        dd("Okey");
+    public function store(Request $request)
+    {
+        $customer_id = CustomerFacade::store($request->all());
+        return redirect()->route('customers.index');
     }
 }
