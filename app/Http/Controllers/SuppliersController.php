8a25d650bd3a0179a69ad40e7dc90f9cc192385d
Amindu-Dulanjana <umamidu@gmail.com>
2025-04-09 22:49:14 +0530

    Supplier Changes

---
+++ app/Http/Controllers/SuppliersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use domain\facades\SupplierFacade\SupplierFacade;
+use Illuminate\Http\Request;
+
+
+class SuppliersController extends ParentController
+{
+    public function index()
+    {
+        $suppliers = SupplierFacade::getSuppliers();
+        return view('pages.suppliers.index', [
+            'suppliers' => $suppliers,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        SupplierFacade::store($request->all());
+        return redirect()->route('suppliers.index');
+    }
+
+    public function get($supplier_id)
+    {
+        $supplier = SupplierFacade::get($supplier_id);
+        return response()->json($supplier);
+    }
+
+    public function update(Request $request, $supplier_id)
+    {
+        return SupplierFacade::update($request->all(), $supplier_id);
+    }
+}
---
+++ resources/views/pages/suppliers/index.blade.php
@@ -0,0 +1,284 @@
+<x-app-layout>
+
+    <div class="app-body">
+
+        <!-- Container starts -->
+        <div class="container">
+
+            <!-- Row start -->
+            <div class="row gx-3">
+                <div class="col-12 col-xl-6">
+                    <!-- Breadcrumb start -->
+                    <ol class="breadcrumb mb-3">
+                        <li class="breadcrumb-item">
+                            <i class="icon-house_siding lh-1"></i>
+                            <a href="{{ route('dashboard') }}" class="text-decoration-none">Home</a>
+                        </li>
+                        <li class="breadcrumb-item">Suppliers</li>
+                    </ol>
+                    <!-- Breadcrumb end -->
+                </div>
+            </div>
+            <!-- Row end -->
+
+            <!-- Row start -->
+            <div class="row gx-3">
+                <div class="col-12">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <!-- Search container start -->
+                            <div class="row mb-3">
+                                <div class="col-10">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Search" />
+                                    </div>
+                                </div>
+                                <div class="col-2 text-end">
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#addNewSupplierModal">Add new</button>
+                                </div>
+                            </div>
+                            <!-- Search container end -->
+
+                            <div class="table-outer">
+                                <div class="table-responsive">
+                                    <table class="table table-striped align-middle m-0">
+                                        <thead>
+                                            <tr>
+                                                <th></th>
+                                                {{--  <th></th>  --}}
+                                                <th>Name</th>
+                                                <th>NIC</th>
+                                                {{--  <th>Status</th>  --}}
+                                                {{-- <th>Mobile</th> --}}
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @if ($suppliers->count() > 0)
+                                                @foreach ($suppliers as $supplier)
+                                                    <tr>
+                                                        <td>{{ $supplier->id }}</td>
+                                                        {{--  <th>
+                                                            <input class="form-check-input" type="checkbox"
+                                                                value="option1" />
+                                                        </th>  --}}
+                                                        <td>
+                                                            {{--  <img src="assets/images/user2.png" class="me-2 img-3x rounded-3"
+                                                        alt="Bootstrap Gallery" />  --}}
+                                                            {{ $supplier->name }}
+                                                        </td>
+                                                        <td>{{ $supplier->nic }}</td>
+                                                        {{--  <td>
+                                                    <div class="d-flex align-items-center">
+                                                        <i class="icon-circle1 me-2 text-success fs-5"></i>
+                                                        Online
+                                                    </div>
+                                                </td>  --}}
+                                                        <td>
+                                                            <button
+                                                                class="btn btn-outline-primary btn-sm edit-customer-btn"
+                                                                onclick="showCustomerEditModal({{ $supplier->id }})"
+                                                                data-bs-toggle="tooltip" data-bs-placement="top"
+                                                                data-bs-custom-class="custom-tooltip-primary"
+                                                                data-bs-title="Edit">
+                                                                <i class="icon-edit"></i>
+                                                            </button>
+                                                            <button class="btn btn-outline-danger btn-sm"
+                                                                data-bs-toggle="tooltip" data-bs-placement="top"
+                                                                data-bs-custom-class="custom-tooltip-danger"
+                                                                data-bs-title="Delete">
+                                                                <i class="icon-trash"></i>
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @endif
+
+                                            {{--  <tr>
+                                                <td>3</td>
+                                                <th>
+                                                    <input class="form-check-input" type="checkbox" value="option3" />
+                                                </th>
+                                                <td>
+                                                    <img src="assets/images/user.png" class="me-2 img-3x rounded-3"
+                                                        alt="Bootstrap Gallery" />
+                                                    Gino Watson
+                                                </td>
+                                                <td>info@example.com</td>
+                                                <td>200</td>
+                                                <td>
+                                                    <div class="d-flex align-items-center">
+                                                        <i class="icon-circle1 me-2 text-dark fs-5"></i>
+                                                        Offline
+                                                    </div>
+                                                </td>
+                                                <td>Turkey</td>
+                                                <td>76</td>
+                                                <td>44</td>
+                                                <td>
+                                                    <div class="starReadOnly1 rating-stars"></div>
+                                                </td>
+                                                <td>
+                                                    <button class="btn btn-outline-primary btn-sm"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top"
+                                                        data-bs-custom-class="custom-tooltip-primary"
+                                                        data-bs-title="Edit">
+                                                        <i class="icon-edit"></i>
+                                                    </button>
+                                                    <button class="btn btn-outline-danger btn-sm"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top"
+                                                        data-bs-custom-class="custom-tooltip-danger"
+                                                        data-bs-title="Delete">
+                                                        <i class="icon-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>  --}}
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Row end -->
+
+        </div>
+        <!-- Container ends -->
+
+    </div>
+
+    <!-- Modals -->
+
+    <!-- Customer add modal start -->
+    <div class="modal fade" id="addNewSupplierModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        Add new supplier
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form method="post" action="{{ route('suppliers.store') }}">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="m-2">
+                            <label class="form-label fw-bold">Name</label>
+                            <input type="text" class="form-control mt-2" placeholder="Enter Name" name="name" />
+                            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2 text-danger" />
+                        </div>
+
+                        <div class="m-2">
+                            <label class="form-label fw-bold">NIC</label>
+                            <input type="text" class="form-control mt-2" placeholder="Enter NIC" name="nic" />
+                            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2 text-danger" />
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Close
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Customer add modal end -->
+
+    <!-- Customer edit modal start -->
+    <div class="modal fade" id="editSupplierModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="editSupplierModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editSupplierModalLabel">
+                        Edit Supplier
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="m-2">
+                        <label class="form-label fw-bold">Name</label>
+                        <input type="text" class="form-control mt-2" placeholder="Enter Name" name="name"
+                            id="edit_name" />
+                        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2 text-danger" />
+                    </div>
+
+                    <div class="m-2">
+                        <label class="form-label fw-bold">NIC</label>
+                        <input type="text" class="form-control mt-2" placeholder="Enter NIC" id="edit_nic"
+                            name="nic" />
+                        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2 text-danger" />
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button onclick="updateSupplier()" class="btn btn-primary">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Customer edit modal end -->
+
+    <script>
+        var edit_customer_id = 0;
+
+        async function showCustomerEditModal(id) {
+            try {
+                const response = await axios.get("{{ url('/customers/get') }}/" + id);
+                const customer = response.data;
+
+                var name = document.getElementById("edit_name");
+                var email = document.getElementById("edit_email");
+                var mobile = document.getElementById("edit_mobile");
+                var address = document.getElementById("edit_address");
+                name.value = customer.name;
+                email.value = customer.email;
+                mobile.value = customer.mobile;
+                address.value = customer.address;
+                edit_customer_id = customer.id;
+
+                // Show Bootstrap modal
+                const modal = new bootstrap.Modal(document.getElementById("editCustomerModal"));
+                modal.show();
+            } catch (error) {
+                console.error(error);
+                alert("Failed to fetch payment data.");
+            }
+        }
+
+        async function updateSupplier() {
+            var name = document.getElementById("edit_name").value;
+            var nic = document.getElementById("edit_nic").value;
+
+            edit_supplier_details = {
+                name: name,
+                nic: nic,
+            }
+
+            try {
+                const response = await axios.post("{{ url('/customers/update') }}/" + edit_customer_id,
+                    edit_customer_details);
+                const customer = response.data;
+
+                window.location.reload();
+            } catch (error) {
+                console.error(error);
+                alert("Failed to fetch payment data.");
+            }
+        }
+    </script>
+</x-app-layout>
