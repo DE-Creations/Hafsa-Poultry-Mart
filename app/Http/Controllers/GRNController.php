224d209d789709587ea62315dc4af93ee9a625ad
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-05-29 19:18:28 +0530

    grn changes

--- app/Http/Controllers/GRNController.php
+++ app/Http/Controllers/GRNController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Grn;
 use Illuminate\Http\Request;
 
 class GRNController extends ParentController
@@ -13,6 +14,51 @@ class GRNController extends ParentController
 
     public function create()
     {
+        $response['date'] = now()->format('Y-m-d');
+        $response['grn_no'] = Grn::generateGrnNumber();
         return view('pages.grn.create');
+    }
+
+    public function loadGRNs(Request $request)
+    {
+        $query = Grn::query();
+
+        // if (isset($request['search'])) {
+        //     $query = $query->where('code', 'like', '%' . $request['search'] . '%')
+        //         ->orWhere('description', 'like', '%' . $request['search'] . '%')
+        //         ->orWhere('note', 'like', '%' . $request['search'] . '%')
+        //         ->orWhere('date', 'like', '%' . $request['search'] . '%')
+        //         ->orWhere('amount', 'like', '%' . $request['search'] . '%');
+        // }
+
+        if (isset($request['count'])) {
+            $response['invoices'] = $query->orderBy('id', 'desc')->paginate($request['count']);
+        } else {
+            $response['invoices'] = $query->orderBy('id', 'desc')->paginate(20);
+        }
+
+        return view('pages.grn.components.table')->with($response);
+    }
+
+    public function store(StoreInvoiceRequest $request)
+    {
+        // if ($request->hasFile('file')) {
+        //     $file = $request->file('file');
+        //     $fileName = time() . '.' . $file->extension();
+        //     $file->move(public_path('storage/expenses'), $fileName);
+        //     $request['file_path'] = 'storage/expenses/' . $fileName;
+        // }
+        return InvoiceFacade::store($request->all());
+    }
+
+    public function edit($invoice_id)
+    {
+        $invoice = InvoiceFacade::get($invoice_id);
+        return view('pages.invoice.edit')->with(['invoice' => $invoice]);
+    }
+
+    public function delete($invoice_id)
+    {
+        return InvoiceFacade::delete($invoice_id);
     }
 }
