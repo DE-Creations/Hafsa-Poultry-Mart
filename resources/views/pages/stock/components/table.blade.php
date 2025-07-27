67df60cb659e3ba0521c06b89aa8bd576bc9fccc
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-07-27 16:22:58 +0530

    stock table view changes

--- resources/views/pages/stock/components/table.blade.php
+++ resources/views/pages/stock/components/table.blade.php
@@ -1,39 +1,43 @@
 <table class="table table-striped align-middle m-0">
     <thead>
         <tr>
+            <th>Id</th>
             <th>Item</th>
-            <th>Price</th>
-            <th>Qty</th>
+            <th>Date</th>
+            <th>Unit Price</th>
+            <th>Balance</th>
             <th>Action</th>
         </tr>
     </thead>
     <tbody>
-        {{ $stocks }}
         @foreach ($stocks as $stock)
             <tr>
                 <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->id }}</td>
-                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->id }}</td>
-                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->id }}</td>
+                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->outputItem->name }}</td>
+                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">
+                    {{ $stock->created_at->format('Y-m-d') }}</td>
+                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->unit_price }}</td>
+                <td style="cursor: pointer" onclick="viewInvoice({{ $stock->id }})">{{ $stock->balance }}</td>
                 {{--  <td style="cursor: pointer" onclick="viewInvoice({{ $invoice->id }})">
-                    {{ $invoice->invoicePayment->first()->new_balance ?? 'N/A' }}</td>
-                <td>  --}}
-                {{--  <?php
-                // if the invoice is relevant customer's last invoice, show the edit and delete buttons. otherwise, do not show them
-                $isLastInvoice = $invoice->customer->stocks->last()->id === $invoice->id;
-                ?>
-                @if ($isLastInvoice)
-                    <button class="btn btn-outline-primary btn-sm" onclick="goToInvoiceEdit({{ $invoice->id }})"
+                    {{ $invoice->invoicePayment->first()->new_balance ?? 'N/A' }}</td> --}}
+                <td>
+                    {{--  <?php
+                    // if the invoice is relevant customer's last invoice, show the edit and delete buttons. otherwise, do not show them
+                    $isLastInvoice = $invoice->customer->stocks->last()->id === $invoice->id;
+                    ?>
+                @if ($isLastInvoice)  --}}
+                    <button class="btn btn-outline-primary btn-sm" onclick="goToInvoiceEdit({{ $stock->id }})"
                         data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-primary"
                         data-bs-title="Edit">
                         <i class="icon-edit"></i>
                     </button>
-                    <button class="btn btn-outline-danger btn-sm" onclick="showDeleteInvoiceModal({{ $invoice->id }})"
+                    <button class="btn btn-outline-danger btn-sm" onclick="showDeleteInvoiceModal({{ $stock->id }})"
                         data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-danger"
                         data-bs-title="Delete">
                         <i class="icon-trash"></i>
                     </button>
-                @endif
-                </td>  --}}
+                    {{--  @endif  --}}
+                </td>
             </tr>
         @endforeach
     </tbody>
