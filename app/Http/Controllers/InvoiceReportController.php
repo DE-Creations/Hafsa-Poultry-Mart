1942c1e977987672ad3f19312b375d77d3dee72b
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-11-16 21:05:09 +0530

    invoice print changes

--- app/Http/Controllers/InvoiceReportController.php
+++ app/Http/Controllers/InvoiceReportController.php
@@ -62,12 +62,12 @@ class InvoiceReportController extends ParentController
             $query->where('customer_id', $customer);
         }
 
-        //$total = $query('sub_total');
         $invoices = $query->with(['customer', 'invoicePayment'])->orderBy('id', 'desc')->get();
 
-        // $pdf = PDF::loadView('print.pages.invoice_report.report', compact('invoices', 'from', 'to', 'customer'));
-        // $pdf->setPaper('A4', 'portrait');
-        // return $pdf->stream('invoice.pdf', ['Attachment' => false]);
+        if ($customer !== 'select') {
+            $customerModel = CustomerFacade::get($customer);
+            $customer = $customerModel->name;
+        }
 
         $pdf = PDF::loadView('print.pages.invoice_report.report', compact('invoices', 'from', 'to', 'customer'));
         $pdf->setPaper('A4', 'portrait');
