23b2438cb35135f3f347896c2ce910180a874880
Sasindu De Silva <sasindudineth1111@gmail.com>
2025-12-06 03:14:45 +0530

    GRN changes

--- resources/views/pages/grn/components/table.blade.php
+++ resources/views/pages/grn/components/table.blade.php
@@ -12,31 +12,35 @@
     </thead>
     <tbody>
         @foreach ($grns as $grn)
-        <tr class="text-center">
-            <td>{{ $grn->grn_number }}</td>
-            <td style="text-align: left; padding-left: 4%;">{{ $grn->supplier->name }}</td>
-            <td>{{ $grn->date }}</td>
-            <td style="text-align: right; padding-right: 4%;">{{ $grn->sub_total }}</td>
-            <td style="text-align: right; padding-right: 4%;">{{ $grn->grnPay->paid_amount}}</td>
-            <td style="text-align: right; padding-right: 4%;">{{ $grn->grnPay->new_balance }}</td>
-            <td>
-                <button class="btn btn-outline-secondary btn-sm" onclick="printGrn('{{ $grn->id }}')"
-                    data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-danger"
-                    data-bs-title="Print">
-                    <i class="icon-printer"></i>
-                </button>
-                <button class="btn btn-outline-primary btn-sm" onclick="goToGrnEdit('{{ $grn->id }}')"
-                    data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-primary"
-                    data-bs-title="Edit">
-                    <i class="icon-edit"></i>
-                </button>
-                <button class="btn btn-outline-danger btn-sm" onclick="showDeleteGRNModal('{{ $grn->id }}')"
-                    data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-danger"
-                    data-bs-title="Delete">
-                    <i class="icon-trash"></i>
-                </button>
-            </td>
-        </tr>
+            <tr class="text-center">
+                <td onclick="viewGrn('{{ $grn->id }}')">{{ $grn->grn_number }}</td>
+                <td onclick="viewGrn('{{ $grn->id }}')" style="text-align: left; padding-left: 4%;">
+                    {{ $grn->supplier->name }}</td>
+                <td onclick="viewGrn('{{ $grn->id }}')">{{ $grn->date }}</td>
+                <td onclick="viewGrn('{{ $grn->id }}')" style="text-align: right; padding-right: 4%;">
+                    {{ $grn->sub_total }}</td>
+                <td onclick="viewGrn('{{ $grn->id }}')" style="text-align: right; padding-right: 4%;">
+                    {{ $grn->grnPay->paid_amount }}</td>
+                <td onclick="viewGrn('{{ $grn->id }}')" style="text-align: right; padding-right: 4%;">
+                    {{ $grn->grnPay->new_balance }}</td>
+                <td>
+                    <button class="btn btn-outline-secondary btn-sm" onclick="printGrn('{{ $grn->id }}')"
+                        data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-danger"
+                        data-bs-title="Print">
+                        <i class="icon-printer"></i>
+                    </button>
+                    <button class="btn btn-outline-primary btn-sm" onclick="goToGrnEdit('{{ $grn->id }}')"
+                        data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-primary"
+                        data-bs-title="Edit">
+                        <i class="icon-edit"></i>
+                    </button>
+                    <button class="btn btn-outline-danger btn-sm" onclick="showDeleteGRNModal('{{ $grn->id }}')"
+                        data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip-danger"
+                        data-bs-title="Delete">
+                        <i class="icon-trash"></i>
+                    </button>
+                </td>
+            </tr>
         @endforeach
     </tbody>
 </table>
